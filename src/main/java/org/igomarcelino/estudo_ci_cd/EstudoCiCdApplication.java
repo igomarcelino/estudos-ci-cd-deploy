f12e81081109373e50534b7c894745a86036a2c2
Igo Marcelino <geslucian@hotmail.com>
2025-09-12 17:36:02 -0300

    feat: commit inicial, configura os profiles com a url do banco de dados

---
+++ src/main/java/org/igomarcelino/estudo_ci_cd/EstudoCiCdApplication.java
@@ -0,0 +1,13 @@
+package org.igomarcelino.estudo_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/igomarcelino/estudo_ci_cd/EstudoCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.igomarcelino.estudo_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
